794ffa5dec79c0861cefa4d7ca414831fe0e1051
Jayden <jaydenang2@gmail.com>
2025-11-19 18:26:22 -0800

    Configure Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Personal from './components/personal';
 import './App.css';
 
-function App() {
+function App(data) {
+  const [personal, setPersonal] = useState(data.personal);
+  const [education, setEducation] = useState(data.education);
+  const [skills, setSkills] = useState(data.skills);
+  const [projects, setProjects] = useState(data.projects);
+  const [work, setWork] = useState(data.work);
+  const [volunteer, setVolunteer] = useState(data.volunteer);
+
+  const personalList = personal.map((p) => (
+    <Personal 
+      name={p.name}
+      email={p.email}
+      phone={p.phone}
+      address={p.address}
+      link={p.link}
+    />
+  ));
+
+  const educationList = education.map((e) => (
+    <Education 
+      institute={e.institute}
+      address={e.address}
+      enrollment={e.enrollment}
+      study={e.study}
+    />
+  ));
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );

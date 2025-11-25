bc963547bf1ba03ce54f27482074573d0b14fbda
Jayden <jaydenang2@gmail.com>
2025-11-25 11:24:34 -0800

    Add documentation page

--- client/src/components/view.jsx
+++ client/src/components/view.jsx
@@ -1,4 +1,4 @@
-import '../index.css';
+import '../App.css';
 import React, { forwardRef } from 'react';
 
 const View = forwardRef(({ personal, education, skills, projects, work, volunteer,
@@ -9,7 +9,7 @@ const View = forwardRef(({ personal, education, skills, projects, work, voluntee
       <div className='header'>
         {personal.map((p, index) => (
           <div key={index}>
-            <strong>{p.name}</strong> <button onClick={() => deletePersonal && deletePersonal(index)} style={{marginLeft:8}}>Delete</button> <br />
+            <strong>{p.name}</strong> <button className='btn' onClick={() => deletePersonal && deletePersonal(index)}>Delete</button> <br />
             {p.address} | {p.phone} | {p.email} <br />
             {p.link && p.link.length > 0 && (
               <ul>
@@ -26,7 +26,7 @@ const View = forwardRef(({ personal, education, skills, projects, work, voluntee
         <h2>Education</h2>
         {education.map((e, index) => (
           <div key={index}>
-            <strong>{e.institute}</strong> <button onClick={() => deleteEducation && deleteEducation(index)} style={{marginLeft:8}}>Delete</button>, {e.address} <br />
+            <strong>{e.institute}</strong>, {e.address} <button className='btn'onClick={() => deleteEducation && deleteEducation(index)}>Delete</button><br />
             ({e.enrollStart} - {e.enrollEnd})<br />
             <ul>
               <li>Studying {e.study}</li>
@@ -38,7 +38,7 @@ const View = forwardRef(({ personal, education, skills, projects, work, voluntee
       <section>
         <h2>Skills: </h2>
           {skills.map((s, index) => (
-            <p key={index}>{s.skill} <button onClick={() => deleteSkill && deleteSkill(index)} style={{marginLeft:8}}>Delete</button> </p>
+            <p key={index}>{s.skill} <button className='btn' onClick={() => deleteSkill && deleteSkill(index)}>Delete</button> </p>
           ))}
       </section>
 
@@ -46,7 +46,7 @@ const View = forwardRef(({ personal, education, skills, projects, work, voluntee
         <h2>Projects</h2>
         {projects.map((pj, index) => (
           <div key={index}>
-            <strong>{pj.title}</strong> <button onClick={() => deleteProject && deleteProject(index)} style={{marginLeft:8}}>Delete</button> <br />
+            <strong>{pj.title}</strong> <button className='btn' onClick={() => deleteProject && deleteProject(index)}>Delete</button> <br />
             ({pj.termStart} - {pj.termEnd}) <br />
             {pj.description && pj.description.length > 0 && (
               <ul>
@@ -63,7 +63,7 @@ const View = forwardRef(({ personal, education, skills, projects, work, voluntee
         <h2>Work Experience</h2>
         {work.map((w, index) => (
           <div key={index}>
-            <strong>{w.position}</strong> at {w.company} <button onClick={() => deleteWork && deleteWork(index)} style={{marginLeft:8}}>Delete</button> <br />
+            <strong>{w.position}</strong> at {w.company} <button className='btn' onClick={() => deleteWork && deleteWork(index)} style={{marginLeft:8}}>Delete</button> <br />
             {w.termStart} - {w.termEnd} <br />
             {w.responsibilities && w.responsibilities.length > 0 && (
               <ul>
@@ -80,7 +80,7 @@ const View = forwardRef(({ personal, education, skills, projects, work, voluntee
         <h2>Volunteer Experience</h2>
         {volunteer.map((v, index) => (
           <div key={index}>
-            <strong>{v.position}</strong> at {v.organization} <button onClick={() => deleteVolunteer && deleteVolunteer(index)} style={{marginLeft:8}}>Delete</button> <br />
+            <strong>{v.position}</strong> at {v.organization} <button className='btn' onClick={() => deleteVolunteer && deleteVolunteer(index)} style={{marginLeft:8}}>Delete</button> <br />
             {v.startTerm} - {v.endTerm} <br />
             {v.responsibilities && v.responsibilities.length > 0 && (
               <ul>

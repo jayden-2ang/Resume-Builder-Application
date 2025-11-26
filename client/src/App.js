51f5b640ec6759f81aac21c5fea5ec29aea91c8e
Jayden <jaydenang2@gmail.com>
2025-11-26 13:38:15 -0800

    Add navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Documentation from './pages/Documentation';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/documentation" element={<Documentation />} />
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper">
+            <h1 className="logo">Resume Builder React App</h1>
+            <ul className="nav-links">
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/Documentation'>Documentation</Link></li>
+            </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
